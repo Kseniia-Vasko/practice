2bf18e6aba6fbb5afb63d9b7cea569b6e52559ea
Ksenia-Verpatova <52948491+Ksenia-Verpatova@users.noreply.github.com>
2019-07-16 12:52:56 +0600

    add 4 functions

--- calk_oop/calk_oop/Form1.Designer.cs
+++ calk_oop/calk_oop/Form1.Designer.cs
@@ -45,6 +45,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -54,6 +55,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -81,6 +83,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -88,10 +91,12 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label1.Location = new System.Drawing.Point(311, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 13);
--- calk_oop/calk_oop/Form1.cs
+++ calk_oop/calk_oop/Form1.cs
@@ -24,12 +24,44 @@ namespace calk_oop
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            double firstArgument = Convert.ToDouble(textBox1.Text);
+            double secondArgument = Convert.ToDouble(textBox2.Text);
+            double result = firstArgument * secondArgument;
+            label1.Text = result.ToString();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            double firstArgument = Convert.ToDouble(textBox1.Text);
+            double secondArgument = Convert.ToDouble(textBox2.Text);
+            double result = firstArgument / secondArgument;
+            label1.Text = result.ToString();
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double firstArgument = Convert.ToDouble(textBox1.Text);
+            double secondArgument = Convert.ToDouble(textBox2.Text);
+            double result = firstArgument + secondArgument;
+            label1.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double firstArgument = Convert.ToDouble(textBox1.Text);
+            double secondArgument = Convert.ToDouble(textBox2.Text);
+            double result = firstArgument - secondArgument;
+            label1.Text = result.ToString();
         }
     }
 }

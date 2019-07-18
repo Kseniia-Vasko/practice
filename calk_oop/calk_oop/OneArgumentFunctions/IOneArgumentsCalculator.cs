1ecc73fdabb879601476082de8083f646cec5c48
Acedia19 <schoolidol@mail.ru>
2019-07-18 11:57:19 +0600

    Remove unused usings

--- calk_oop/calk_oop/OneArgumentFunctions/IOneArgumentsCalculator.cs
+++ calk_oop/calk_oop/OneArgumentFunctions/IOneArgumentsCalculator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace calk_oop.OneArgumentFunctions
+﻿namespace calk_oop.OneArgumentFunctions
 {
     public interface IOneArgumentsCalculator
     {
--- calk_oop/calk_oop/TwoArgumentFunctions/ITwoArgumentsCalculator.cs
+++ calk_oop/calk_oop/TwoArgumentFunctions/ITwoArgumentsCalculator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace calk_oop.TwoArgumentFunctions
+﻿namespace calk_oop.TwoArgumentFunctions
 {
     public interface ITwoArgumentsCalculator
     {

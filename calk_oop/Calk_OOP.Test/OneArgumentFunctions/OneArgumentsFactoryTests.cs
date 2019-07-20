00585ef50ea1a698b0243d8f6ffba9e8fe807be2
Ksenia-Verpatova <valkyrja.ks@gmail.com>
2019-07-20 11:24:32 +0600

    Add One and Two Factorys tests

---
+++ calk_oop/Calk_OOP.Test/OneArgumentFunctions/OneArgumentsFactoryTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using calk_oop.OneArgumentFunctions;
+using NUnit.Framework;
+
+namespace Calk_OOP.Test.OneArgumentFunctions
+{
+    [TestFixture]
+
+    class OneArgumentsFactoryTests
+    {
+        [TestCase("Sin", typeof(Sinus))]
+        [TestCase("Cos", typeof(Cosinus))]
+        [TestCase("Exp", typeof(Exponenta))]
+        [TestCase("Tan", typeof(Tangens))]
+        [TestCase("Arcsin", typeof(Arcsinus))]
+        [TestCase("Arccos", typeof(Arccosinus))]
+        [TestCase("Arctan", typeof(Arctangens))]
+        [TestCase("Abs", typeof(Mod))]
+        [TestCase("Ln", typeof(LogarithmN))]
+        [TestCase("Lg", typeof(Logarithm10))]
+        [TestCase("Neg", typeof(Negation))]
+        [TestCase("Pw", typeof(Power2))]
+        [TestCase("Pwr", typeof(Pow10))]
+        [TestCase("Back", typeof(Divideback))]
+        [TestCase("Pwer", typeof(Anotherpower))]
+        [TestCase("Ctg", typeof(Cotangens))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentsFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
---
+++ calk_oop/Calk_OOP.Test/TwoArgumentFunctions/TwoArgumentFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using calk_oop.TwoArgumentFunctions;
+using NUnit.Framework;
+
+namespace Calk_OOP.Test.TwoArgumentFunctions
+{
+    [TestFixture]
+
+    class TwoArgumentFactoryTests
+    {
+        [TestCase("Add", typeof(Adder))]
+        [TestCase("Subtraction", typeof(Subtracter))]
+        [TestCase("Multiplication", typeof(Multipier))]
+        [TestCase("Divide", typeof(Divider))]
+        [TestCase("Power", typeof(PowerB))]
+        [TestCase("Log", typeof(Logarithm))]
+        [TestCase("Min", typeof(Minimum))]
+        [TestCase("Max", typeof(Maximum))]
+        [TestCase("Avg", typeof(Average))]
+        [TestCase("Pow", typeof(Power))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentsFactory.CreateCalculator(name);
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
